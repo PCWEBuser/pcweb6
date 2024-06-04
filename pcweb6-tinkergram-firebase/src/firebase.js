0f67aac4fd793ac82333e8b09eb6f6fe1a6a111a
pcwebuser <r530anniversary@gmail.com>
2024-06-04 16:06:11 +0800

    update firebase

--- pcweb6-tinkergram-firebase/src/firebase.js
+++ pcweb6-tinkergram-firebase/src/firebase.js
@@ -13,9 +13,10 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_ESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
--- pcweb6-tinkergram-firebase/src/views/LoginPage.js
+++ pcweb6-tinkergram-firebase/src/views/LoginPage.js
@@ -4,14 +4,11 @@ import { Button, Container, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 
-
-
 export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-const navigate = useNavigate();
-
+  const navigate = useNavigate();
   return (
     <Container>
       <h1 className="my-3">Login to your account</h1>
@@ -55,9 +52,8 @@ const navigate = useNavigate();
         >
           Login
         </Button>
-
       </Form>
-      <p></p>
+      <p>{error}</p>
     </Container>
   );
 }
